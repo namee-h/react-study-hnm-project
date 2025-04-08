c01cba3e15564579660834dd604cecf7dc18db77
Sara han <hans63237@gmail.com>
2025-04-09 00:56:07 +0900

    api 요청

--- src/page/ProductAll.jsx
+++ src/page/ProductAll.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import ProductCard from '../conponent/ProductCard'
 
 const ProductAll = () => {
+    const [productList, setProductList] = useState([])
+    const getProducts = async()=>{
+        let url = "http://localhost:5000/products"
+        let res = await fetch(url)
+        let data = await res.json()
+        // console.log("data",data)
+        setProductList(data)
+    } 
+    useEffect(()=>{
+        getProducts()
+    },[])
   return (
     <div>
-    
+    {productList.map((item)=><ProductCard item={item}/>)}
     </div>
   )
 }
